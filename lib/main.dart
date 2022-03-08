7e52bbaf813b73ac368c0ed8873b287e2057e783
MizanurRemon <remonmizanur@gmail.com>
2022-03-08 13:14:03 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() {
+  runApp(const myApp());
+}
+
+var counter = 0;
+
+// ignore: camel_case_types
+class myApp extends StatelessWidget {
+  const myApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: const Color.fromARGB(99, 78, 9, 124),
+            leading: IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                show_toast("menu");
+              },
+            ),
+            title: const Text("Home Page"),
+            centerTitle: true,
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.camera),
+                onPressed: () {
+                  show_toast("camera");
+                },
+              )
+            ],
+          ),
+          body: const HomeClass(),
+        ),
+      ),
+    );
+  }
+}
+
+// ignore: non_constant_identifier_names
+void show_toast(var type) {
+  Fluttertoast.showToast(
+    msg: type,
+  );
+
+  //const HomeClass();
+  //myHomePageState(counter.toString());
+}
+
+class HomeClass extends StatefulWidget {
+  const HomeClass({Key? key}) : super(key: key);
+
+  @override
+  // ignore: no_logic_in_create_state
+  State<HomeClass> createState() => myHomePageState();
+}
+
+// ignore: camel_case_types
+class myHomePageState extends State<HomeClass> {
+  int a = 0;
+  void increaseFunc() {
+    setState(() {
+      a++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Center(
+            child: TextButton(
+                onPressed: () {
+                  show_toast("Its a TextView");
+                },
+                child: Text(
+                  "message " + a.toString() + " times",
+                  style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 0, 0, 0)),
+                )),
+          ),
+        ),
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: FloatingActionButton(
+            onPressed: increaseFunc,
+            tooltip: 'Increment',
+            child: const Icon(
+              Icons.message,
+              color: Color.fromARGB(98, 255, 255, 255),
+            ),
+            backgroundColor: const Color.fromARGB(99, 78, 9, 124),
+          ),
+        ));
+  }
+}
